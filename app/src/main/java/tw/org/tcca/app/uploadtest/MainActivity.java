547e70d2930cf72f12c6e1bd88f8a7e36db59c4a
Brad <brad@brad.tw>
2019-08-15 18:55:32 +0800

    1

---
+++ app/src/main/java/tw/org/tcca/app/uploadtest/MainActivity.java
@@ -0,0 +1,40 @@
+package tw.org.tcca.app.uploadtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    private File sdroot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    123);
+        }
+
+        sdroot = Environment.getExternalStorageDirectory();
+
+    }
+
+
+    public void test1(View view) {
+
+    }
+}
